c3fcac0076a55fa1e435929ae71ea6359dfd996b
Vilde Vevatne <vilde@vevatne.no>
2017-04-16 18:35:47 -0700

    Adds authentications to controllers

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "name", password: "password", except: [:index, :show]
+
   def index
     @articles = Article.all
   end

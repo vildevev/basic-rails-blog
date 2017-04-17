36e0c046b16c5d00c4ae81769861007d1bea9848
Vilde Vevatne <vilde@vevatne.no>
2017-04-16 18:15:49 -0700

    Adds edit link to view pages

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -21,6 +21,15 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def update
+  @article = Article.find(params[:id])
+
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+
   def show
     @article = Article.find(params[:id])
   end
